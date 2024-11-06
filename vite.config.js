b4b2899bd1794c39989d999a253e7123158a9a2f
Yevheniia Tobolovska <142045878+tobolovskaya@users.noreply.github.com>
2024-11-06 12:14:52 +0100

    1

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
   root: 'src',
+  base: '/your-energy/',
   build: {
     rollupOptions: {
       input: glob.sync('./src/*.html'),
